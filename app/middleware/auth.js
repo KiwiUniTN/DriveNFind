6ef3c6f98b878614944559a277a263c87c5bd8a9
Leoconfortiuni <152866468+Leoconfortiuni@users.noreply.github.com>
2024-11-13 21:03:36 +0100

    modifiche auth e users route v4

--- app/middleware/auth.js
+++ app/middleware/auth.js
@@ -5,7 +5,7 @@ export function authorizeRole(allowedRoles) {
   return async (req) => {
     const token = req.headers.get('authorization')?.split(' ')[1];
     if (!token) {
-      return NextResponse.json({ message: 'Authorization token required' }, { status: 401 });
+      return Response.json({ message: 'Authorization token required' }, { status: 401 });
     }
 
     try {
@@ -13,13 +13,13 @@ export function authorizeRole(allowedRoles) {
       console.log(decoded.username)
 
       if (!allowedRoles.includes(decoded.role)) {
-        return NextResponse.json({ message: 'Forbidden - insufficient permissions' }, { status: 403 });
+        return Response.json({ message: 'Forbidden - insufficient permissions' }, { status: 403 });
       }
 
       req.user = decoded;
       return true;  // Authorization success
     } catch (error) {
-      return NextResponse.json({ message: 'Invalid token' }, { status: 401 });
+      return Response.json({ message: 'Invalid token' }, { status: 401 });
     }
   };
 }
