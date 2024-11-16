c1aeb90d34f216e94592c1b1f8ba4953678c3850
filippoArcariUNI <filippo.arcari@studenti.unitn.it>
2024-11-16 21:01:55 +0100

    report di user funzionano in base ai ruoli

--- app/api/users/baseusers/reports/route.js
+++ app/api/users/baseusers/reports/route.js
@@ -1,88 +1,76 @@
 import { authorizeRole } from "@/app/middleware/auth";
-import Report  from "@/app/models/Report";
+import Report from "@/app/models/Report";
 /* crea un report dato nel body della richiesta 
     "parkingLotId": id del parcheggio,
     "description": "descrizione del problema",
-    "status": "In sospeso",
     "imageUrl": "path/to/image" */
+
 export async function POST(req, res) {
-    // Authenticate user based on the provided token
+	// Authenticate user based on the provided token
     let userAuth;
+    let body;
+	try {
+		userAuth= await validateUser(req);
+		body = await req.json();
+        console.log("userAuth" ,userAuth)
+	} catch (error) {
+		return new Response(JSON.stringify({ message: error.message }), {
+			status: 403,
+		});
+	}
+
+	const newReport = {
+		username: userAuth.user.username,
+		parkingLotId: body.parkingLotId,
+		description: body.description,
+		status: "In sospeso",
+		imageUrl: body.imageUrl,
+	};
+
+	try {
+		const createdReport = await Report.create(newReport);
+		return new Response(JSON.stringify(createdReport), { status: 201 });
+	} catch (error) {
+		return new Response(
+			JSON.stringify({ message: `Report creation failed:${error.message}` }),
+			{ status: 500 }
+		);
+	}
+	
+}
+/* elimina una segnalazione dato l'Id messo nel body in questo modo {"id" : <id>} */
+export async function DELETE(req, res) {
+    
     const body = await req.json();
     try {
-        userAuth = await authorizeRole(["baseuser"])(req);
-        if (userAuth.authorized !== true) {
-					throw new Error("Unauthorized");
-				}
+        await validateUser(req);
     } catch (error) {
-        return new Response(JSON.stringify({ message: error.message }), { status: 401 });
+        return new Response(JSON.stringify({ message: error.message }), {
+            status: 403,
+        });
     }
-    if (userAuth.user.role === "admin") {
-        return new Response(JSON.stringify({ message: 'Admin users cannot create reports' }), { status: 403 });
-    }
-    
-    const newReport = {
-			username: userAuth.user.username,
-			parkingLotId: body.parkingLotId,
-			description: body.description,
-			status: body.status,
-			imageUrl: body.imageUrl,
-		};
-	
-     try {
-			const createdReport = await Report.create(newReport);
-			return new Response(JSON.stringify(createdReport), { status: 201 });
-		} catch (error) {
-			return new Response(
-				JSON.stringify({ message: `Report creation failed:${error.message}` }),{ status: 500 }
-			);
-		}
+
+	const deletionSuccess = await Report.findByIdAndDelete(body.id);
+	if (deletionSuccess) {
+		return new Response(
+			JSON.stringify({ message: "Report deleted successfully" }),
+			{ status: 200 }
+		);
+	} else {
+		return new Response(JSON.stringify({ message: "Report deletion failed" }), {
+			status: 500,
+		});
+	}
 }
 
-export async function PATCH(req, res) {
-    const { reportId } = req.query;
-    const { description, imageUrl } = req.body;
-
-    // Authenticate user based on the provided token
-    const isUser = await authorizeRole(["user"])(req);
-    if (isUser !== true) return res.status(401).json({ error: "Unauthorized" });
-
-    if (req.user.role === "admin") {
-        return res.status(403).json({ error: "Admin users cannot update reports" });
-    }
-
-    if (!reportId) {
-        return res.status(400).json({ error: "Report ID is required" });
-    }
-
-    const updatedData = { description, imageUrl };
-    const updatedReport = await Report.findByIdAndUpdate(reportId, updatedData, { new: true });
-    if (updatedReport) {
-        return res.status(200).json(updatedReport);
-    } else {
-        return res.status(404).json({ error: "Report not found or update failed" });
-    }
-}
-
-export async function DELETE(req, res) {
-    const { reportId } = req.query;
-
-    // Authenticate user based on the provided token
-    const isUser = await authorizeRole(["user"])(req);
-    if (isUser !== true) return res.status(401).json({ error: "Unauthorized" });
-
-    if (req.user.role === "admin") {
-        return res.status(403).json({ error: "Admin users cannot delete reports" });
-    }
-
-    if (!reportId) {
-        return res.status(400).json({ error: "Report ID is required" });
-    }
-
-    const deletionSuccess = await Report.findByIdAndDelete(reportId);
-    if (deletionSuccess) {
-        return res.status(204).end();
-    } else {
-        return res.status(404).json({ error: "Report not found or deletion failed" });
-    }
+// controllo se l'utente è un baseuser
+async function validateUser(req) {
+	const userAuth = await authorizeRole(["baseuser"])(req);
+	if (!userAuth.authorized) {
+		throw new Error("User not authorized");
+	}
+	if (userAuth.user.role === "admin") {
+		throw new Error("Admins cannot create reports or delete them");
+	}
+	return userAuth;
 }
