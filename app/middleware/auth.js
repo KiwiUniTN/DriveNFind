60a446e210bdc130604a497537f7d64233c09366
Kevin Gelmini <147542831+KiwiUniTN@users.noreply.github.com>
2024-11-16 09:41:06 +0100

    auth ora ritorna {authorized,user (payload del token),Response del middleware}. Da Next 15 req.user = decoded non era persistente

--- app/middleware/auth.js
+++ app/middleware/auth.js
@@ -1,22 +1,22 @@
 import jwt from 'jsonwebtoken';
-
 export function authorizeRole(allowedRoles) {
   return async (req) => {
     const token = req.headers.get('authorization')?.split(' ')[1];
-    if (!token) {
-      return Response.json({ message: 'Authorization token required' }, { status: 401 });//Throw error if no token is present
+    console.log('Authorization token:', token);
+    if (!token) {// If no token is provided, return an error response
+      return { authorized: false, response: Response.json({ message: 'Authorization token required' }, { status: 401 }) };
     }
 
     try {
       const decoded = jwt.verify(token, process.env.JWT_SECRET);
-      if (!allowedRoles.includes(decoded.role)) {
-        return Response.json({ message: 'Forbidden - insufficient permissions' }, { status: 403 });//Throw error if user does not have the required role
+
+      if (!allowedRoles.includes(decoded.role)) {// If the user does not have the required role, return an error response
+        return { authorized: false, response: Response.json({ message: 'Forbidden - insufficient permissions' }, { status: 403 }) };
       }
 
-      req.user = decoded;
-      return true;  // Authorization success
+      return { authorized: true, user: decoded };// If the user is authorized, return the decoded token
     } catch (error) {
-      return Response.json({ message: 'Invalid token' }, { status: 401 });
+      return { authorized: false, response: Response.json({ message: 'Invalid token' }, { status: 401 }) };
     }
   };
 }
