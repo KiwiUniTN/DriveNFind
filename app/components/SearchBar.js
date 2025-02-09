7e55bb5eff45101c0c9b708cb060ac280eef0ca0
filippoArcariUNI <filippo.arcari@studenti.unitn.it>
2025-02-09 16:21:15 +0100

    ancora merge error

--- app/components/SearchBar.js
+++ app/components/SearchBar.js
@@ -3,78 +3,76 @@ import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faMagnifyingGlass, faFilter } from "@fortawesome/free-solid-svg-icons";
 
 function getAPIStringfromFilters(filters, freeOnly) {
-  const mapFilters = {
-    pagamento: "regolamento=pagamento,pagamento-disco orario",
-    gratis: "regolamento=disco orario,gratuito senza limitazione d'orario",
-    disabili: "disabile=true",
-    elettrico: "alimentazione=elettrico",
-    tipologia: "tipologia=coperto"
-  };
+	const mapFilters = {
+		pagamento: "regolamento=pagamento,pagamento-disco orario",
+		gratis: "regolamento=disco orario,gratuito senza limitazione d'orario",
+		disabili: "disabile=true",
+		elettrico: "alimentazione=elettrico",
+		tipologia: "tipologia=coperto",
+	};
 
-  let query = "";
-  for (const filter in filters) {
-    if (filters[filter]) {
-      query += mapFilters[filter] + "&";
-    }
-  }
+	let query = "";
+	for (const filter in filters) {
+		if (filters[filter]) {
+			query += mapFilters[filter] + "&";
+		}
+	}
 
-  if (freeOnly) {
-    query += "disponibilita=libero&";
-  }
+	if (freeOnly) {
+		query += "disponibilita=libero&";
+	}
 
-  query = query.slice(0, -1); // Remove trailing "&"
-  return query;
+	query = query.slice(0, -1); // Remove trailing "&"
+	return query;
 }
 
 const SearchBar = ({ refreshSpots, position, cardSpots }) => {
-  const [query, setQuery] = useState("");
-  const [freeOnly, setFreeOnly] = useState(true);
-  const [suggestions, setSuggestions] = useState([]);
-  const [showSuggestions, setShowSuggestions] = useState(false);
-  const [isLoading, setIsLoading] = useState(false);
+	const [query, setQuery] = useState("");
+	const [freeOnly, setFreeOnly] = useState(true);
+	const [suggestions, setSuggestions] = useState([]);
+	const [showSuggestions, setShowSuggestions] = useState(false);
+	const [isLoading, setIsLoading] = useState(false);
 
-  const [filters, setFilters] = useState({
-    pagamento: false,
-    gratis: false,
-    disabili: false,
-    elettrico: false,
-    tipologia: false,
-  });
+	const [filters, setFilters] = useState({
+		pagamento: false,
+		gratis: false,
+		disabili: false,
+		elettrico: false,
+		tipologia: false,
+	});
 
-  // Initialize the parking spots list
-  useEffect(() => {
-    refreshSpots("");
-  }, []);
+	// Initialize the parking spots list
+	useEffect(() => {
+		refreshSpots("");
+	}, []);
 
-  // Handle checkbox changes
-  const handleCheckboxChange = (filterName) => {
-    setFilters((prevFilters) => {
-      if (filterName === "pagamento" || filterName === "gratis") {
-        const oppositeFilter = filterName === "pagamento" ? "gratis" : "pagamento";
-        return {
-          ...prevFilters,
-          [filterName]: !prevFilters[filterName],
-          [oppositeFilter]: false
-        };
-      } else {
-        return {
-          ...prevFilters,
-          [filterName]: !prevFilters[filterName],
-        };
-      }
-    });
-  };
+	// Handle checkbox changes
+	const handleCheckboxChange = (filterName) => {
+		setFilters((prevFilters) => {
+			if (filterName === "pagamento" || filterName === "gratis") {
+				const oppositeFilter =
+					filterName === "pagamento" ? "gratis" : "pagamento";
+				return {
+					...prevFilters,
+					[filterName]: !prevFilters[filterName],
+					[oppositeFilter]: false,
+				};
+			} else {
+				return {
+					...prevFilters,
+					[filterName]: !prevFilters[filterName],
+				};
+			}
+		});
+	};
 
 	useEffect(() => {
 		const query = getAPIStringfromFilters(filters, freeOnly);
 		refreshSpots(query);
-	}, [filters, freeOnly]); // React when `freeOnly` state changes and filters changes
-	
-	//Google Places API quando avremo i soldi
-	// Fetch location suggestions from Nominatim
+	}, [filters, freeOnly]);
+
 	const fetchLocations = async (searchTerm) => {
 		setIsLoading(true);
-
 		try {
 			const response = await fetch(
 				`https://nominatim.openstreetmap.org/search?format=json&countrycodes=IT&addressdetails=1&polygon=1&bounded=1&viewbox=${
@@ -83,11 +81,9 @@ const SearchBar = ({ refreshSpots, position, cardSpots }) => {
 					position[0] + 0.25
 				}&q=${searchTerm}`
 			);
-
-			if (!response.ok) throw new Error(`HTTP error! Status: ${response.status}`);
-
+			if (!response.ok)
+				throw new Error(`HTTP error! Status: ${response.status}`);
 			const data = await response.json();
-			console.log("Suggestions:", data);
 			setSuggestions(data);
 		} catch (error) {
 			console.error("Error fetching locations:", error);
@@ -96,7 +92,7 @@ const SearchBar = ({ refreshSpots, position, cardSpots }) => {
 			setIsLoading(false);
 		}
 	};
-	// Handle input change
+
 	const handleInputChange = (e) => {
 		const value = e.target.value.trim();
 		setQuery(value);
@@ -104,90 +100,84 @@ const SearchBar = ({ refreshSpots, position, cardSpots }) => {
 			setShowSuggestions(false);
 			setSuggestions([]);
 		}
-
 	};
 
-  const handleIconClick = async () => {
-    if (!query.trim()) return;
-    setIsLoading(true);
-    try {
-      await fetchLocations(query);
-    } finally {
-      setIsLoading(false);
-    }
-  };
+	const handleIconClick = async () => {
+		if (!query.trim()) return;
+		setIsLoading(true);
+		try {
+			await fetchLocations(query);
+		} finally {
+			setIsLoading(false);
+		}
+	};
 
-  const handleLocationSelect = (location) => {
-    setQuery(location.display_name);
-    setSuggestions([]);
-    setShowSuggestions(false);
-    const { lat, lon } = location;
-    let queryAPI = getAPIStringfromFilters(filters, freeOnly);
-    queryAPI += lat ? `&lat=${lat}` : "";
-    queryAPI += lon ? `&long=${lon}` : "";
-    refreshSpots(queryAPI).then((res) => { cardSpots(res) });
-  };
+	const handleLocationSelect = (location) => {
+		setQuery(location.display_name);
+		setSuggestions([]);
+		setShowSuggestions(false);
+		const { lat, lon } = location;
+		let queryAPI = getAPIStringfromFilters(filters, freeOnly);
+		queryAPI += lat ? `&lat=${lat}` : "";
+		queryAPI += lon ? `&long=${lon}` : "";
+		refreshSpots(queryAPI).then((res) => {
+			cardSpots(res);
+		});
+	};
 
-  const uniqueSuggestions = Array.from(
-    new Map(suggestions.map(item => [item.address.road, item])).values()
-  );
+	const uniqueSuggestions = Array.from(
+		new Map(suggestions.map((item) => [item.address.road, item])).values()
+	);
 
-  return (
-    <div className='flex rounded-box items-center gap-2 join'>
-      <label className='input input-bordered flex items-center gap-2 bg-white'>
-        <input
-          type='text'
-          className='grow text-black raleway-regular'
-          placeholder='Cerca la tua destinazione'
-          onChange={handleInputChange}
-        />
-        {!isLoading ? (
-          <FontAwesomeIcon
-            icon={faMagnifyingGlass}
-            className='text-beigeChiaro h-5 w-5 hover:text-beigeChiaro'
-            onClick={handleIconClick}
-          />
-        ) : (<span className='loading loading-spinner loading-xs'></span>)}
-      </label>
+	return (
+		<div className='flex rounded-box items-center gap-2 join'>
+			<label className='input input-bordered flex items-center gap-2 bg-white'>
+				<input
+					type='text'
+					className='grow text-black raleway-regular'
+					placeholder='Cerca la tua destinazione'
+					onChange={handleInputChange}
+				/>
+				{!isLoading ? (
+					<FontAwesomeIcon
+						icon={faMagnifyingGlass}
+						className='text-beigeChiaro h-5 w-5 hover:text-beigeChiaro'
+						onClick={handleIconClick}
+					/>
+				) : (
+					<span className='loading loading-spinner loading-xs'></span>
+				)}
+			</label>
 
-      {uniqueSuggestions.length > 0 && (
-        <ul className='raleway-regular absolute top-12 left-0 w-full mt-2 bg-white border rounded shadow-md z-50 max-h-40 overflow-auto'>
-          {uniqueSuggestions.map((location, index) => (
-            <li
-              key={index}
-              className='p-2 hover:bg-gray-200 cursor-pointer'
-              onClick={() => handleLocationSelect(location)}>
-              {location.address.aeroway} {location.address.railway} {location.address.building} {location.address.road} {location.address.county}
-            </li>
-          ))}
-        </ul>
-      )}
+			{uniqueSuggestions.length > 0 && (
+				<ul className='raleway-regular absolute top-12 left-0 w-full mt-2 bg-white border rounded shadow-md z-50 max-h-40 overflow-auto'>
+					{uniqueSuggestions.map((location, index) => (
+						<li
+							key={index}
+							className='p-2 hover:bg-gray-200 cursor-pointer'
+							onClick={() => handleLocationSelect(location)}>
+							{location.address.aeroway} {location.address.railway}{" "}
+							{location.address.building} {location.address.road}{" "}
+							{location.address.county}
+						</li>
+					))}
+				</ul>
+			)}
 
 			<details className='dropdown rounded-box'>
 				<summary className='btn bg-white border-none hover:bg-slate-900'>
 					<FontAwesomeIcon
 						icon={faFilter}
-						className='text-gray h-5 w-5 bg-wh '
+						className='text-gray h-5 w-5 bg-wh'
 					/>
 				</summary>
-				<ul className='menu dropdown-content bg-white rounded-box z-[1]  w-52'>
-					<li>
-						<div className='form-control'>
-							<label className='label cursor-pointer justify-between w-40 '>
-								<span className='label-text text-black raleway-regular'>Tutti</span>
-								<input
-									type='checkbox'
-									className='checkbox'
-									checked={filters.tutti}
-									onChange={() => handleCheckboxChange("tutti")}
-								/>
-							</label>
-						</div>
-					</li>
+				<ul className='menu dropdown-content bg-white rounded-box z-[1] w-52'>
 					<li>
 						<div className='form-control'>
 							<label className='label cursor-pointer flex justify-between w-40'>
-								<span className='label-text text-black raleway-regular'>A Pagamento</span>
+								<span className='label-text text-black raleway-regular'>
+									A Pagamento
+								</span>
 								<input
 									type='checkbox'
 									className='checkbox'
@@ -200,7 +190,9 @@ const SearchBar = ({ refreshSpots, position, cardSpots }) => {
 					<li>
 						<div className='form-control'>
 							<label className='label cursor-pointer flex justify-between w-40'>
-								<span className='label-text text-black raleway-regular'>Gratuito</span>
+								<span className='label-text text-black raleway-regular'>
+									Gratuito
+								</span>
 								<input
 									type='checkbox'
 									className='checkbox'
@@ -213,7 +205,9 @@ const SearchBar = ({ refreshSpots, position, cardSpots }) => {
 					<li>
 						<div className='form-control'>
 							<label className='label cursor-pointer flex justify-between w-40'>
-								<span className='label-text text-black raleway-regular'>Per Disabili</span>
+								<span className='label-text text-black raleway-regular'>
+									Per Disabili
+								</span>
 								<input
 									type='checkbox'
 									className='checkbox'
@@ -226,7 +220,9 @@ const SearchBar = ({ refreshSpots, position, cardSpots }) => {
 					<li>
 						<div className='form-control'>
 							<label className='label cursor-pointer flex justify-between w-40'>
-								<span className='label-text text-black raleway-regular'>Elettrico</span>
+								<span className='label-text text-black raleway-regular'>
+									Elettrico
+								</span>
 								<input
 									type='checkbox'
 									className='checkbox'
@@ -239,7 +235,9 @@ const SearchBar = ({ refreshSpots, position, cardSpots }) => {
 					<li>
 						<div className='form-control'>
 							<label className='label cursor-pointer flex justify-between w-40'>
-								<span className='label-text text-black raleway-regular'>Coperto</span>
+								<span className='label-text text-black raleway-regular'>
+									Coperto
+								</span>
 								<input
 									type='checkbox'
 									className='checkbox'
@@ -251,10 +249,12 @@ const SearchBar = ({ refreshSpots, position, cardSpots }) => {
 					</li>
 				</ul>
 			</details>
-			<div className="flex items-center gap-1 flex-col">
-				<span className="text-nowrap text-xs text-gray-600">Parcheggi Liberi</span>
-				<input type="checkbox" className="toggle toggle-success" checked={freeOnly} onChange={() => setFreeOnly(!freeOnly)} />
-			</div>
+			<input
+				type='checkbox'
+				className='toggle checked:bg-[#a0b536] toggle-success'
+				checked={freeOnly}
+				onChange={() => setFreeOnly(!freeOnly)}
+			/>
 		</div>
 	);
 };
